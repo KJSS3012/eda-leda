aa227358544aad76a9c14c2b174c98064d17a8c0
Kaique Santos <kaiquesantos.dev@gmail.com>
2026-06-05 15:41:35 -0300

    fix: Duplicated Class error in RemoveIndexLinkedList

--- src/main/java/br/edu/ufcg/eda/tst/lds/RemoveIndexLinkedList.java
+++ src/main/java/br/edu/ufcg/eda/tst/lds/RemoveIndexLinkedList.java
@@ -4,11 +4,16 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class RemoveIndexLinkedList {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int[] values = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] values = Arrays.stream(in.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
         int index = Integer.parseInt(in.nextLine());
+
         LinkedList lista = new LinkedList();
 
         for (int value : values) {
@@ -19,107 +24,139 @@ class RemoveIndexLinkedList {
 
         System.out.println(lista.toString().trim());
     }
-}
 
-class LinkedList {
-    protected Node head;
-    protected Node tail;
-    private int size;
+    static class LinkedList {
 
-    public LinkedList() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
+        protected Node head;
+        protected Node tail;
+        private int size;
 
-    public boolean isEmpty() {
-        return this.head == null;
-    }
-
-    public void addLast(int value) {
-        Node nn = new Node(value);
-        if (isEmpty()) {
-            this.head = nn;
-        } else {
-            nn.prev = this.tail;
-            this.tail.next = nn;
+        public LinkedList() {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
         }
-        this.tail = nn;
-        this.size++;
-    }
 
-    public int remove(int index) {
-        Node aux = this.head;
+        public boolean isEmpty() {
+            return this.head == null;
+        }
 
-        if (index == 0) {
-            return removeFirst();
-        } else if (index == size - 1) {
-            return removeLast();
-        } else {
-            for (int i = 0; i < index; i++) {
-                aux = aux.next;
+        public void addLast(int value) {
+
+            Node nn = new Node(value);
+
+            if (isEmpty()) {
+                this.head = nn;
+                this.tail = nn;
+
+            } else {
+                nn.prev = this.tail;
+                this.tail.next = nn;
+                this.tail = nn;
             }
 
-            aux.prev.next = aux.next;
-            aux.next.prev = aux.prev;
-            this.size -= 1;
+            this.size++;
+        }
+
+        public int remove(int index) {
+
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException();
+            }
+
+
+            Node aux = this.head;
+
+
+            if (index == 0) {
+                return removeFirst();
+
+            } else if (index == size - 1) {
+                return removeLast();
+
+            } else {
+
+                for (int i = 0; i < index; i++) {
+                    aux = aux.next;
+                }
+
+                aux.prev.next = aux.next;
+                aux.next.prev = aux.prev;
+
+                size--;
+
+                return aux.value;
+            }
+        }
+
+        public int removeFirst() {
+
+            Node aux = this.head;
+
+            if (aux.next == null) {
+
+                this.head = null;
+                this.tail = null;
+
+            } else {
+
+                this.head = aux.next;
+                this.head.prev = null;
+            }
+
+            size--;
 
             return aux.value;
         }
-    }
 
-    public int removeFirst() {
-        Node aux = this.head;
+        public int removeLast() {
 
-        if (aux.next == null) {
-            this.head = null;
-            this.tail = null;
-        } else {
-            aux.next.prev = null;
-            this.head = aux.next;
-        }
-        this.size -= 1;
-        return aux.value;
-    }
+            Node aux = this.tail;
 
-    public int removeLast() {
-        Node aux = this.tail;
+            if (aux.prev == null) {
 
-        if (aux.prev == null) {
-            this.head = null;
-            this.tail = null;
-        } else {
-            aux.prev.next = null;
-            this.tail = aux.prev;
-        }
-        this.size -= 1;
-        return aux.value;
-    }
+                this.head = null;
+                this.tail = null;
 
-    public int size() {
-        return this.size;
-    }
+            } else {
 
-    public String toString() {
-        Node aux = this.head;
-        String answer = "";
-        while (aux != null) {
-            answer += aux.value + " ";
-            aux = aux.next;
+                this.tail = aux.prev;
+                this.tail.next = null;
+            }
+
+            size--;
+
+            return aux.value;
         }
 
-        return  answer;
-    }
-}
+        public int size() {
+            return this.size;
+        }
 
-class Node {
-    protected Node next;
-    protected Node prev;
-    protected int value;
+        public String toString() {
 
-    public Node(int value) {
-        this.value = value;
-        this.next = null;
-        this.prev = null;
+            Node aux = this.head;
+            String answer = "";
+
+            while (aux != null) {
+                answer += aux.value + " ";
+                aux = aux.next;
+            }
+
+            return answer;
+        }
+
+        static class Node {
+
+            protected Node next;
+            protected Node prev;
+            protected int value;
+
+            public Node(int value) {
+                this.value = value;
+                this.next = null;
+                this.prev = null;
+            }
+        }
     }
 }
